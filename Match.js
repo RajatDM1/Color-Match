42f614154d7d563df435af1521367ef5e0392321
bira91 <chilled.bira91@yandex.com>
2017-09-23 15:13:04 +0530

    added new random colors.

--- Match.js
+++ Match.js
@@ -5,7 +5,20 @@ var pickedColor = pickColor();
 var message = document.querySelector("#message");
 //console.log(pickedColor);
 var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor; 
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click",function(){
+colors = generateRandomColors(6);
+pickedColor = pickColor();
 colorDisplay.textContent = pickedColor;
+for(var i=0;i<squares.length;i++)
+{
+	squares[i].style.background = colors[i]; 
+}
+
+});
 for(var i=0;i<squares.length;i++)
 {
 	console.log(pickedColor);
@@ -17,6 +30,7 @@ for(var i=0;i<squares.length;i++)
 		if(clickedColor == pickedColor){
 			message.textContent = "Correct";
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		}
 		else
 		{
